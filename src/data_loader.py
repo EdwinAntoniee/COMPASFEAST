b9cedc2f133438d495bd44a540be2e7e299ff566
Edwin Antonie <edwin.xw23@gmail.com>
2026-08-20 20:55:02 +0700

    pipeline export, cleaning all of the notebooks files

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import os
 
-def load_ai4i_data(filepath="../data/raw/ai4i2020.csv"):
+def load_ai4i_data(filepath=None):
     """Load dataset AI4I"""
-    if not os.path.exists(filepath):
-        filepath = "data/01_raw/ai4i2020.csv"
+    if filepath is None or not os.path.exists(filepath):
+        possible_paths = [
+            "data/raw/ai4i2020.csv",
+            "../data/raw/ai4i2020.csv",
+            "data/01_raw/ai4i2020.csv",
+            "../data/01_raw/ai4i2020.csv"
+        ]
+        for p in possible_paths:
+            if os.path.exists(p):
+                filepath = p
+                break
         
     df = pd.read_csv(filepath)
 
